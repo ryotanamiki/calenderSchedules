27805df54e9f2167e9108e213bc1679d93dca061
r.namiki <aaasti555@gmail.com>
2023-10-12 12:13:52 +0900

    レイアウトとsqlの再設計

--- calender_db.sql
+++ calender_db.sql
@@ -1,8 +1,10 @@
--- Active: 1695798624080@@localhost@3306@calenderscheDb
-CREATE TABLE schedules(  
+CREATE DATABASE calendersche_db;
+use calendersche_db;
+
+CREATE Table schedule (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date DATE NOT NULL,
     title VARCHAR(255) NOT NULL,
     user VARCHAR(255) NOT NULL,
     content TEXT
-) COMMENT '';
+);
